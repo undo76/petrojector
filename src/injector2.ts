766baf36c087cf403e11be26baa65bc7cc7f7186
Manolo Santos <manolo.santos@gmail.com>
2021-02-20 13:12:43 +0100

    Rename partial -> inject

--- src/injector2.ts
+++ src/injector2.ts
@@ -18,9 +18,9 @@ function Content({ content }: ContentProps): Markup {
   return `<main>${content}</main>`;
 }
 
-type AppProps = { header: Markup; content: Markup; footer: Markup };
+type PageProps = { header: Markup; content: Markup; footer: Markup };
 
-function App({ header, content, footer }: AppProps): Markup {
+function Page({ header, content, footer }: PageProps): Markup {
   return `
     <div>
         ${header} 
@@ -30,53 +30,55 @@ function App({ header, content, footer }: AppProps): Markup {
 `;
 }
 
-const myApp2 = App({
+const myPage = Page({
   header: Header({ welcomeMessage: "Welcome!" }),
   content: Content({ content: "Lorem ipsum dolor..." }),
   footer: Footer({ year: 2021 }),
 });
 
-console.log(myApp2);
+console.log(myPage);
 
-// We would like to create a template with the header and footer injected, and the content missing.
-// The idea is to inject the content later.
+// We would like to create a template with the header and footer injected as
+// to inject the content later.
 
 // This fails as content is missing
-// const Skeleton = App({
+// const Skeleton = Page({
 //     header: Header({ welcomeMessage: "Welcome!" }),
 //     footer: Footer({ year: 2021 }),
 // });
 
-// This function creates a partial component with the passed props applied.
-// It can be applied several times. Typing works, but I think it could be improved.
-function partial<T extends P, P, R>(component: (props: T) => R, props: P) {
+// This function inject properties to the component, returning a new type-safe
+// partially applied component. It can be applied several times.
+// Typing works, but there's room for improvement.
+function inject<T extends P, P, R>(component: (props: T) => R, props: P) {
   return function (props2: Omit<T, keyof P> | T = {} as T): R {
     let allProps = { ...props, ...props2 };
     return component(allProps as T); // TODO: This cast seems unnecessary
   };
 }
 
-const AppTemplate = partial(App, {
+// Create a Page template with the header and footer already applied.
+const PageTemplate = inject(Page, {
   header: Header({ welcomeMessage: "Welcome!" }),
   footer: Footer({ year: 2021 }),
 });
 
-// Doesn't work, needs "content"
+// This doesn't work, as it needs "content" to be filled in too.
 // const out = AppTemplate({});
 // console.log(out);
 
 // Add content
-const out2 = AppTemplate({ content: "adios" });
-console.log(out2);
+const page2 = PageTemplate({ content: "adios" });
+console.log(page2);
 
 // Override header
-const out3 = AppTemplate({ header: "Header overriden!", content: "wiiiiii" });
-console.log(out3);
+const page3 = PageTemplate({ header: "Header overriden!", content: "wiiiiii" });
+console.log(page3);
 
-// Apply partial to the template to add content and footer.
-const FullApp = partial(AppTemplate, {
+// Apply partial again, creating a new Partial with all the properties
+const FullPageTemplate = inject(PageTemplate, {
   content: "That's all folks",
   footer: Footer({ year: 1976 }),
 });
-const out4 = FullApp();
+const out4 = FullPageTemplate();
 console.log(out4);
